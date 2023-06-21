398ff39e77531738cdb48bfd33aa4243c76d1578
Manohar <manoharacharya63@gmail.com>
2023-06-21 16:48:19 +0530

    search icon issue

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/common/colors.dart';
+import 'package:food_delivery/widget/round_textfield.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -8,8 +10,106 @@ class HomeView extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<HomeView> {
+  TextEditingController txtSearch = TextEditingController();
+
+  List catArr = [
+    {"image": "", "name": ""},
+    {"image": "", "name": ""},
+    {"image": "", "name": ""},
+    {"image": "", "name": ""},
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color(0XFFF5F5F5),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Good morning Manohar!",
+                        style: TextStyle(
+                          color: TColor.primaryText,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Image.asset(
+                          "assets/img/shopping_cart.png",
+                          width: 25,
+                          height: 25,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Delivering to",
+                        style: TextStyle(
+                          color: TColor.secondaryText,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          Text(
+                            "Current Location",
+                            style: TextStyle(
+                              color: TColor.secondaryText,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(width: 25),
+                          Image.asset(
+                            "assets/img/dropdown.png",
+                            width: 15,
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 20),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 0),
+                        child: RoundTextfield(
+                          hintText: "Search Food",
+                          controller: txtSearch,
+                          left: Container(
+                            alignment: Alignment.center,
+                            width: 30,
+                            child: Image.asset(
+                              "assets/img/search.png",
+                              width: 20,
+                              height: 20,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/widget/round_textfield.dart
+++ lib/widget/round_textfield.dart
@@ -7,6 +7,8 @@ class RoundTextfield extends StatelessWidget {
     this.controller,
     this.keyboardType,
     this.obscureText = false,
+    this.bgcolor,
+    this.left,
     required this.hintText,
   });
 
@@ -14,30 +16,45 @@ class RoundTextfield extends StatelessWidget {
   final String hintText;
   final TextInputType? keyboardType;
   final bool obscureText;
+  final Color? bgcolor;
+  final Widget? left;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: TColor.textfiels,
+        color: bgcolor ?? TColor.textfiels,
         borderRadius: BorderRadius.circular(25),
       ),
-      child: TextField(
-        autocorrect: false,
-        controller: controller,
-        obscureText: obscureText,
-        keyboardType: keyboardType,
-        decoration: InputDecoration(
-          contentPadding: const EdgeInsets.symmetric(horizontal: 20),
-          enabledBorder: InputBorder.none,
-          focusedBorder: InputBorder.none,
-          hintText: hintText,
-          hintStyle: TextStyle(
-            color: TColor.placeholder,
-            fontSize: 14,
-            fontWeight: FontWeight.w500,
+      child: Row(
+        children: [
+          if (left != null)
+            Padding(
+              padding: const EdgeInsets.only(left: 15),
+              child: left!,
+            ),
+          Expanded(
+            child: TextField(
+              autocorrect: false,
+              controller: controller,
+              obscureText: obscureText,
+              keyboardType: keyboardType,
+              decoration: InputDecoration(
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                enabledBorder: InputBorder.none,
+                focusedBorder: InputBorder.none,
+                prefixIcon: left,
+                hintText: hintText,
+                hintStyle: TextStyle(
+                  color: TColor.placeholder,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
